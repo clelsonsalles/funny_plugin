a0c0db213bd4f6bc1d09b31260d9af9b50a732d4
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-05-16 11:12:34 -0300

    Update cliente_controller.rb
    Implementando método create de cliente

--- app/controllers/cliente_controller.rb
+++ app/controllers/cliente_controller.rb
@@ -19,12 +19,13 @@ class ClienteController < ApplicationController
   end
 
   def create
-    valores = params.require(:contrato).permit(:numero, :objeto, :contratada, :cnpjContratada, :SEIcontratacao, :SEIedital, :SEIcontrato, :observacoes, :prazoContratacao, :valorTotal, :dataAssinatura, :dataPublicacao)
-    contrato = Contrato.create(valores)
+    valoresUsuario = params.require(:contrato).permit(:numero, :objeto, :contratada, :cnpjContratada, :SEIcontratacao, :SEIedital, :SEIcontrato, :observacoes, :prazoContratacao, :valorTotal, :dataAssinatura, :dataPublicacao)
+    valoresCliente = params.require(:contrato).permit(:numero, :objeto, :contratada, :cnpjContratada, :SEIcontratacao, :SEIedital, :SEIcontrato, :observacoes, :prazoContratacao, :valorTotal, :dataAssinatura, :dataPublicacao)
 
-    projeto = Project.find(params[:contrato][:id_projeto])
-    contrato.project = projeto
-    contrato.save
+    usuario = User.create(valoresUsuario)
+    cliente = Cliente.create(valoresUsuario)
+    cliente.usuario = usuario
+    cliente.save
 
     redirect_to contrato_index_path(project_id: projeto.id)
 
