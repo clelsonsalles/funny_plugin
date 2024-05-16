2d19e5dfaa59cb3823d642ebd6f0f66ec7a0de09
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-05-16 10:27:12 -0300

    Update routes.rb
    Inclusão das rotas para clientes

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,9 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
+
+Rails.application.routes.draw do
+    resources :cliente, only: [:new, :create, :destroy, :edit, :update, :index]
+    get 'clientes', :to => 'cliente#list'
+    get 'create_cliente', :to => 'cliente#create'
+
+end
