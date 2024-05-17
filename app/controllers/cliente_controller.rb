890c1aca04c835a25f4021828588439730405d5b
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-05-16 23:16:21 -0300

    Update cliente_controller.rb

--- app/controllers/cliente_controller.rb
+++ app/controllers/cliente_controller.rb
@@ -21,11 +21,27 @@ class ClienteController < ApplicationController
     valoresUsuario = params.require(:cliente_user).permit(:firstname, :lastname, :mail, :login, :password, :password_confirmation)
     valoresCliente = params.require(:cliente_user).permit(:empresaNome)
 
-    usuario = User.create(valoresUsuario)
+    @user = User.new(:language => Setting.default_language,
+                     :mail_notification => Setting.default_notification_option,
+                     :admin => false)
+
+    
+    unless @user.auth_source_id
+      @user.password              = params[:cliente_user][:password]
+      @user.password_confirmation = params[:cliente_user][:password_confirmation]
+    end
+
+    @user.firstname = params[:cliente_user][:firstname]
+    @user.lastname = params[:cliente_user][:lastname]
+    @user.mail = params[:cliente_user][:mail]
+    @user.login = params[:cliente_user][:login]
+    
+    @user.save
+
+      
     cliente = Cliente.create(valoresCliente)
     
-    usuario.save
-    cliente.user = usuario
+    cliente.user = @user
     cliente.save
 
     redirect_to aguarde_path()
