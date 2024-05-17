2e2f87c4d24d1e379401c537e1fcb5f4b9413840
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-05-16 22:39:19 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@
 # See: http://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
-    resources :cliente, only: [:new, :create, :destroy, :edit, :update, :index, :aguarde]
+    resources :cliente, only: [:new, :create, :destroy, :edit, :update, :index]
     get 'clientes', :to => 'cliente#list'
+    get 'aguarde', :to => 'cliente#aguarde'
+
     
 
 end
