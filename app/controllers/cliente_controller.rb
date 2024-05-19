ec7e47ba0cf17d9864898a10a26bdfdb0a0f9391
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-05-19 10:33:15 -0300

    Update cliente_controller.rb
    Inclusão do método register no controller de clientes

--- app/controllers/cliente_controller.rb
+++ app/controllers/cliente_controller.rb
@@ -60,4 +60,47 @@ class ClienteController < ApplicationController
   def find_issues_with_query(query)
   end
 
+
+
+  
+  # User self-registration
+  def register
+    (redirect_to(home_url); return) unless Setting.self_registration? || session[:auth_source_registration]
+    if !request.post?
+      session[:auth_source_registration] = nil
+      @user = User.new(:language => current_language.to_s)
+    else
+      user_params = params[:user] || {}
+      @user = User.new
+      @user.safe_attributes = user_params
+      @user.pref.safe_attributes = params[:pref]
+      @user.admin = false
+      @user.register
+      if session[:auth_source_registration]
+        @user.activate
+        @user.login = session[:auth_source_registration][:login]
+        @user.auth_source_id = session[:auth_source_registration][:auth_source_id]
+        if @user.save
+          session[:auth_source_registration] = nil
+          self.logged_user = @user
+          flash[:notice] = l(:notice_account_activated)
+          redirect_to my_account_path
+        end
+      else
+        unless user_params[:password].blank? && user_params[:password_confirmation].blank?
+          @user.password, @user.password_confirmation = user_params[:password], user_params[:password_confirmation]
+        end
+
+        case Setting.self_registration
+        when '1'
+          register_by_email_activation(@user)
+        when '3'
+          register_automatically(@user)
+        else
+          register_manually_by_administrator(@user)
+        end
+      end
+    end
+  end
+
 end
