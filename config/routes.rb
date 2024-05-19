00ebd362310459a66966e452d4941436798debda
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-05-19 10:18:10 -0300

    Update routes.rb
    Inclusão do register para cliente

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     resources :cliente, only: [:new, :create, :destroy, :edit, :update, :index]
     get 'clientes', :to => 'cliente#list'
     get 'aguarde', :to => 'cliente#aguarde'
+    match 'cliente/register', :to => 'cliente#register', :via => [:get, :post], :as => 'register'
+
 
     
 
