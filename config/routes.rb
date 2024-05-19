68601f43662f0dc8d9de06361622c14a281e4e96
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-05-19 10:58:46 -0300

    Update routes.rb
    Alteração na rota de registro de cliente DE register PARA registrar

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     resources :cliente, only: [:new, :create, :destroy, :edit, :update, :index]
     get 'clientes', :to => 'cliente#list'
     get 'aguarde', :to => 'cliente#aguarde'
-    match 'cliente/register', :to => 'cliente#register', :via => [:get, :post], :as => 'register'
+    match 'cliente/register', :to => 'cliente#register', :via => [:get, :post], :as => 'registrar'
 
 
     
