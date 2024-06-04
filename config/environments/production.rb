edf3c1d32698912970477806d6c26f3f1efaa053
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-06-04 19:00:06 -0300

    Update production.rb
    debug log

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,4 +96,6 @@ Rails.application.configure do
 
   # Automatically execute asset precompilation on startup in case of changes have been detected in assets
   config.assets.redmine_detect_update = true
+
+  config.log_level = :debug
 end
