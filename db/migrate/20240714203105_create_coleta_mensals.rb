719a5461e7ac40942537b1c6d7de2c81dcce6cf7
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-07-14 18:58:19 -0300

    Delete db/migrate/20240714203105_create_coleta_mensals.rb

--- db/migrate/20240714203105_create_coleta_mensals.rb
+++
@@ -1,9 +0,0 @@
-class CreateColetaMensals < ActiveRecord::Migration[6.1]
-  def change
-    create_table :coleta_mensals do |t|
-      t.String :cnpj
-      t.text :razaoSocial
-      t.integer :ano
-    end
-  end
-end
