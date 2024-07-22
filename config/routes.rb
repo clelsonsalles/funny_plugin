4d5f92bc97d9d2287a6903107a772ae5f28064f9
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-07-22 11:14:04 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 
+
+RedmineApp::Application.routes.draw do
+  root :to => 'account#login'
+end
+
+
 Rails.application.routes.draw do
     resources :cliente, only: [:new, :create, :destroy, :edit, :update, :index]
     get 'clientes', :to => 'cliente#list'
