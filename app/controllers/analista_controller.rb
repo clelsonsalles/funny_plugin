3668dacb1450aa1b126196f3c64b33dbf5cf4d0d
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-08-12 12:27:39 -0300

    Update analista_controller.rb

--- app/controllers/analista_controller.rb
+++ app/controllers/analista_controller.rb
@@ -4,6 +4,11 @@ class AnalistaController < ApplicationController
       @projects = User.current.projects.to_a
   end
 
+  def cliente_analista
+    organizacao = Project.find(params[:analista][:id_projeto])
+    membros = organizacao.memberships.sorted.to_a
+  end
+
   def create
   end
   
