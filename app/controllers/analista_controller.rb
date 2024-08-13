54b378c30960aaf05c0b1ffcfeb1ab2ca0ee9e7a
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-08-13 17:31:18 -0300

    Update analista_controller.rb

--- app/controllers/analista_controller.rb
+++ app/controllers/analista_controller.rb
@@ -7,6 +7,13 @@ class AnalistaController < ApplicationController
   def cliente_analista
     @organizacao = Organizacao.new(Project.find(params[:id_projeto]))
 
+    @organizacao.visible_custom_field_values.each do |custom_value|
+        if !custom_value.value.blank? 
+          if custom_value.name == 'CNPJ'
+            @organizacao.cnpj(custom_value.value)
+        end
+    end
+
     
   end
 
