ed07048490b97913eaf5e4f80c1051c4053bb992
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-08-13 11:09:07 -0300

    Update Organizacao.rb

--- app/model/Organizacao.rb
+++ app/model/Organizacao.rb
@@ -2,8 +2,8 @@ class Organizacao
   attr_accessor :nome, :cnpj, :uf, :endereco, :cep, :cidade, :telefone, :email, :servicos, :userResponsavel, :userPreenchimento
 
 
-    def initialize(projeto)
-      self.nome = projeto.name
-    end
+  def inicia(projeto)
+    self.nome = projeto.name
+  end
 
 end
