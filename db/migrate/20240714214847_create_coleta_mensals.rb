4c5c316c75583d53a2c965d5abbf34e4e546e1a7
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-08-14 15:35:55 -0300

    Delete db/migrate/20240714214847_create_coleta_mensals.rb
    Remoção das coletas mensais e semestrais, para utilizar apenas uma classe de coleta

--- db/migrate/20240714214847_create_coleta_mensals.rb
+++
@@ -1,9 +0,0 @@
-class CreateColetaMensals < ActiveRecord::Migration[6.1]
-  def change
-    create_table :coleta_mensals do |t|
-      t.text :razaoSocial
-      t.integer :cnpj
-      t.integer :ano
-    end
-  end
-end
