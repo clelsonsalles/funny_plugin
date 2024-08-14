af8c284e1c2b6065fbf484696dc567e7986b0f78
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-08-14 14:44:38 -0300

    Update usuario.rb
    Recupera o telefone do usuário a partir do campo customizado

--- app/model/usuario.rb
+++ app/model/usuario.rb
@@ -5,6 +5,14 @@ class Usuario
   def initialize(user)
     self.nome = user.name
     self.email = user.mail
+   
+     projeto.custom_field_values.each do |custom_value|
+        if !custom_value.value.blank? 
+            if custom_value.custom_field.name == 'Telefone'
+              self.telefone = custom_value.value.to_s
+            end
+        end
+     end
   end
 
 end
