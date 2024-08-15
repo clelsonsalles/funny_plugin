5b45a17d903cc8664c8348b778f5751ac30ae198
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-08-15 13:37:08 -0300

    Update coleta_mensal_controller.rb

--- app/controllers/coleta_mensal_controller.rb
+++ app/controllers/coleta_mensal_controller.rb
@@ -4,9 +4,16 @@ class ColetaMensalController < ApplicationController
   end
 
   def inserir
-    @coleta.responsavelPreenchimento = User.current
-    @coleta.project = Project.find(params[:id_projeto])
-    @coleta.new
+    valores = params.require(:coleta).permit(:mes)
+    coleta = Coleta.create(valores)
+
+    coleta.tituloColeta = "Coleta Mensal"
+    coleta.tipoColeta = params[:tipoColeta]
+    coleta.ano = 2024
+        
+    coleta.responsavelPreenchimento = User.current
+    coleta.project = Project.find(params[:id_projeto])
+    coleta.save
     
     redirect_to coleta_mensal_criar_path(id_projeto: params[:id_projeto])
   end
