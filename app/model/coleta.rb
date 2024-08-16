bc31645176f5001708241c408ab3c71163b317a9
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-08-15 23:09:24 -0300

    Update coleta.rb

--- app/model/coleta.rb
+++ app/model/coleta.rb
@@ -3,6 +3,8 @@ class Coleta < ActiveRecord::Base
   belongs_to :project
 
   def textoData      
-      self.mes.to_s + "/" + self.ano.to_s
+      nomeMeses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro", "NÃO DEFINIDO"]
+      numeroMes = self.mes < 12 ? self.mes : 13  
+      nomeMeses[numeroMes].to_s + "/" + self.ano.to_s
   end
 end
