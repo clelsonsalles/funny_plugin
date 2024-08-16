fbfd3859bd20033e4b5e027af0b1a90830403207
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-08-15 22:53:39 -0300

    Update coleta.rb

--- app/model/coleta.rb
+++ app/model/coleta.rb
@@ -2,7 +2,7 @@ class Coleta < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
 
-  def textoData
-      self.mes + "/" + self.ano
+  def textoData      
+      self.mes.to_s + "/" + self.ano.to_s
   end
 end
