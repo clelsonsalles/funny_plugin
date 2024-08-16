57a5b601217870e6ce72e89ee71a685dfd513869
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-08-15 22:06:37 -0300

    Update coleta_semestral_controller.rb

--- app/controllers/coleta_semestral_controller.rb
+++ app/controllers/coleta_semestral_controller.rb
@@ -1,8 +1,38 @@
-class ColetaSemestralController < ApplicationController
+class ColetaMensalController < ApplicationController
 
   def index
   end
 
-  def create
+  def inserir
+    valores = params.require(:coleta).permit(:mes, :tipoColeta)
+    coleta = Coleta.create(valores)
+
+    coleta.tituloColeta = "Coleta Semestral"
+    coleta.ano = 2024
+        
+    coleta.user = User.current
+    coleta.project = Project.find(params[:coleta][:id_projeto])
+    coleta.save
+    
+    redirect_to analista_cliente_analista_path(id_projeto: params[:coleta][:id_projeto])
+  end
+
+  def criar
+    @coleta = Coleta.new
+    @projeto = Project.find(params[:id_projeto])
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
