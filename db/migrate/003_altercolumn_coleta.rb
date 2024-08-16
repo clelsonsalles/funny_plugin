3d72a713f4ab3b8ae1a6e74d8cc5c0818b83fdc8
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-08-16 15:26:58 -0300

    Update 003_altercolumn_coleta.rb

--- db/migrate/003_altercolumn_coleta.rb
+++ db/migrate/003_altercolumn_coleta.rb
@@ -1,7 +1,12 @@
 class AddDatasToColeta < ActiveRecord::Migration[6.1]
-  def change
+  def up
     add_column :coleta, :dataCriacao, :datetime
     add_column :coleta, :dataRealizacao, :datetime
     add_column :coleta, :dataInformacaoEnvio, :datetime
   end
+
+  def down
+    # no-op
+  end
+
 end
