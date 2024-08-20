9e7a9e2c1a55982ff0fc7d458b899de7725a76aa
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-08-20 11:41:20 -0300

    Update Organizacao.rb

--- app/model/Organizacao.rb
+++ app/model/Organizacao.rb
@@ -5,6 +5,16 @@ class Organizacao
   def initialize(projeto)
     self.nome = projeto.name
     self.idProject = projeto.id
+
+    projeto.custom_field_values.each do |custom_value|
+        if !custom_value.value.blank? 
+            if custom_value.custom_field.name == 'CNPJ'
+              self.cnpj = custom_value.value.to_s
+            end
+        end
+     end
+
+    
   end
 
 end
