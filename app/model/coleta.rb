7d609d9abceb1c358f3c5ad3e315740f731112ff
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-08-20 09:45:37 -0300

    Update coleta.rb

--- app/model/coleta.rb
+++ app/model/coleta.rb
@@ -6,13 +6,21 @@ class Coleta < ActiveRecord::Base
   def textoData      
       nomeMeses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro", "NÃO DEFINIDO"]
       numeroMes = self.mes < 12 ? self.mes : 13  
-      nomeMeses[numeroMes].to_s + "/" + self.ano.to_s
+      nomeMeses[numeroMes -1].to_s + "/" + self.ano.to_s
   end
 
   def status
       time = Time.new
-      status = self.ano < time.year ? "Atrasada" : 
-                self.mes < time.month ? "Atrasada" : "Aguardando Coleta"
+      if self.dataRealizacao.nil?
+          status = self.ano < time.year ? "Coleta Atrasada" : 
+                    self.mes < time.month ? "Coleta Atrasada" : "Aguardando Coleta"
+      else 
+        if self.dataInformacaoEnvio.nil?
+            status = "Coleta Realizada"
+        else
+            status = "Coleta Enviada"
+        end
+      end
 
       status
   end
