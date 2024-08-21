c1d264b8e1ec4e217d83009b8bc1099ba9724824
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-08-21 17:48:37 -0300

    Update coleta_controller.rb

--- app/controllers/coleta_controller.rb
+++ app/controllers/coleta_controller.rb
@@ -3,8 +3,57 @@ class ColetaController < ApplicationController
   def index
   end
 
-  def mensalvisualizar
+
+  def mensalcriar
+    @coleta = Coleta.new
+    @projeto = Project.find(params[:id_projeto])
   end
+
+  def mensalinserir
+    valores = params.require(:coleta).permit(:mes, :tipoColeta)
+    coleta = Coleta.create(valores)
+
+    coleta.tituloColeta = "Coleta Mensal"
+    coleta.ano = 2024
+        
+    coleta.user = User.current
+    coleta.project = Project.find(params[:coleta][:id_projeto])
+    coleta.save
+    
+    redirect_to analista_cliente_analista_path(id_projeto: params[:coleta][:id_projeto])
+  end
+
+
+
+
+  def mensalfazer
+      @coleta = Coleta.find(params[:id_coleta])     
+
+      @ufs = []
+      @municipios = []
+
+      require 'net/http'
+      require 'json'
+      
+      # URL da API externa de UFs e cidades
+      external_api_url_ufs = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
+      external_api_url_cidades = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados/'
+      
+      # Fazendo a requisição para a API externa
+      uri = URI(external_api_url_ufs)
+      response = Net::HTTP.get(uri)
+      array =  JSON.parse(response)    
+      array.each do |ufJson|
+        # do something with element
+        uf = Uf.new
+        uf.id = ufJson["id"]
+        uf.nome = ufJson["nome"]
+        uf.sigla = ufJson["sigla"]
+        @ufs << uf
+      end
+    
+  end
+
 
   def mensalcidadesuf
       @ufs = []
@@ -48,58 +97,14 @@ class ColetaController < ApplicationController
   end
 
   
-  
-  def updateOLD
-    @coleta = Coleta.find(params[:coleta][:id_coleta])     
+    def mensalinformar
+      @coleta = Coleta.find(params[:coleta][:id])     
+      @coleta.uf = params[:coleta][:uf]
+      @coleta.cidade = params[:coleta][:cidade]
+      @coleta.codigoIBGE = params[:coleta][:codigoIBGE]
 
-    @coleta.safe_attributes = params[:coleta]
-    @coleta.dataRealizacao = Time.current
-    if @coleta.save
-      redirect_to cliente_cliente_path
+ 
     end
-  end
-
-  def mensalinserir
-    valores = params.require(:coleta).permit(:mes, :tipoColeta)
-    coleta = Coleta.create(valores)
-
-    coleta.tituloColeta = "Coleta Mensal"
-    coleta.ano = 2024
-        
-    coleta.user = User.current
-    coleta.project = Project.find(params[:coleta][:id_projeto])
-    coleta.save
-    
-    redirect_to analista_cliente_analista_path(id_projeto: params[:coleta][:id_projeto])
-  end
-
-  def mensalfazer
-      @coleta = Coleta.find(params[:id_coleta])     
-
-      @ufs = []
-      @municipios = []
-
-      require 'net/http'
-      require 'json'
-      
-      # URL da API externa de UFs e cidades
-      external_api_url_ufs = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
-      external_api_url_cidades = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados/'
-      
-      # Fazendo a requisição para a API externa
-      uri = URI(external_api_url_ufs)
-      response = Net::HTTP.get(uri)
-      array =  JSON.parse(response)    
-      array.each do |ufJson|
-        # do something with element
-        uf = Uf.new
-        uf.id = ufJson["id"]
-        uf.nome = ufJson["nome"]
-        uf.sigla = ufJson["sigla"]
-        @ufs << uf
-      end
-    
-  end
 
   def mensalatualizar
 
@@ -111,17 +116,23 @@ class ColetaController < ApplicationController
         redirect_to cliente_cliente_path
       end
   end
-
-    def mensalinformar
-      @coleta = Coleta.find(params[:coleta][:id])     
-
-      @coleta.safe_attributes = params[:coleta]
-  
-    end
     
 
-  def mensalcriar
-    @coleta = Coleta.new
-    @projeto = Project.find(params[:id_projeto])
+
+ 
+  
+  def updateOLD
+    @coleta = Coleta.find(params[:coleta][:id_coleta])     
+
+    @coleta.safe_attributes = params[:coleta]
+    @coleta.dataRealizacao = Time.current
+    if @coleta.save
+      redirect_to cliente_cliente_path
+    end
   end
+
+ 
+  def mensalvisualizar
+  end
+
 end
