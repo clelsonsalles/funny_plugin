3acc1563bd9265234df6168d345ab1d95a3ccae0
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-08-21 13:08:40 -0300

    Update coleta_controller.rb

--- app/controllers/coleta_controller.rb
+++ app/controllers/coleta_controller.rb
@@ -100,7 +100,7 @@ class ColetaController < ApplicationController
 
   end
   
-  def update
+  def updateOLD
     @coleta = Coleta.find(params[:coleta][:id_coleta])     
 
     @coleta.safe_attributes = params[:coleta]
@@ -129,7 +129,13 @@ class ColetaController < ApplicationController
   end
 
   def mensalatualizar
-    
+    @coleta = Coleta.find(params[:coleta][:id_coleta])     
+
+    @coleta.safe_attributes = params[:coleta]
+    @coleta.dataRealizacao = Time.current
+    if @coleta.save
+      redirect_to cliente_cliente_path
+    end
   end
 
   def mensalcriar
