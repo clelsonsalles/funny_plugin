0e1c5ae5ffdc8af8fcb56f0a35d9f5b1e33fc9f6
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-08-23 09:34:22 -0300

    Update export_csv.rb

--- lib/export_csv.rb
+++ lib/export_csv.rb
@@ -3,15 +3,13 @@ require 'csv'
 
 module Redmine
   module ExportCSV
-    def self.export_coletas_to_csv(coletas)
+    def self.export_coleta_to_csv(coleta)
       CSV.generate(headers: true) do |csv|
         # Adiciona o cabeçalho
         csv << ['ID', 'tituloColeta', 'tipoColeta', 'ano', 'mes', 'trimestre', 'uf', 'cidade', 'codigoIBGE', 'tipoCliente', 'tipoAtendimento', 'tipoMeio', 'tipoTecnologia', 'tipoProduto', 'velocidade', 'quantidadeAcesso', 'dadoInformado', 'valor', 'cn']
 
-        # Adiciona as linhas
-        coletas.each do |coleta|
-          csv << [coleta.id, coleta.tituloColeta, coleta.tipoColeta, coleta.ano, coleta.mes, coleta.trimestre, coleta.uf, coleta.cidade, coleta.codigoIBGE, coleta.tipoCliente, coleta.tipoAtendimento, coleta.tipoMeio, coleta.tipoTecnologia, coleta.tipoProduto, coleta.velocidade, coleta.quantidadeAcesso, coleta.dadoInformado, coleta.valor, coleta.cn]
-        end
+        # Adiciona a coleta
+        csv << [coleta.id, coleta.tituloColeta, coleta.tipoColeta, coleta.ano, coleta.mes, coleta.trimestre, coleta.uf, coleta.cidade, coleta.codigoIBGE, coleta.tipoCliente, coleta.tipoAtendimento, coleta.tipoMeio, coleta.tipoTecnologia, coleta.tipoProduto, coleta.velocidade, coleta.quantidadeAcesso, coleta.dadoInformado, coleta.valor, coleta.cn]
       end
     end
   end
