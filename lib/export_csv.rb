4067e0bc99d6114320a20a48f7ab8d5f22147b29
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-08-23 10:42:58 -0300

    Update export_csv.rb

--- lib/export_csv.rb
+++ lib/export_csv.rb
@@ -10,5 +10,42 @@ module ExportCsv
         # Adiciona a coleta
         csv << [coleta.id, coleta.tituloColeta, coleta.tipoColeta, coleta.ano, coleta.mes, coleta.trimestre, coleta.uf, coleta.cidade, coleta.codigoIBGE, coleta.tipoCliente, coleta.tipoAtendimento, coleta.tipoMeio, coleta.tipoTecnologia, coleta.tipoProduto, coleta.velocidade, coleta.quantidadeAcesso, coleta.dadoInformado, coleta.valor, coleta.cn]
       end
+
+      class Base < ::CSV
+        include Redmine::I18n
+
+        class << self
+          def generate(options = {}, &)
+            col_sep = (options[:field_separator].presence || l(:general_csv_separator))
+            encoding = Encoding.find(options[:encoding]) rescue Encoding.find(l(:general_csv_encoding))
+
+            str =
+              if encoding == Encoding::UTF_8
+                +"\xEF\xBB\xBF" # BOM
+              else
+                (+'').force_encoding(encoding)
+              end
+
+            super(str, :col_sep => col_sep, :encoding => encoding, &)
+          end
+        end
+
+        def <<(row)
+          row = row.map do |field|
+            case field
+            when String
+              Redmine::CodesetUtil.from_utf8(field, self.encoding.name)
+            when Float
+              @decimal_separator ||= l(:general_csv_decimal_separator)
+              ("%.2f" % field).gsub('.', @decimal_separator)
+            else
+              field
+            end
+          end
+          super
+        end
+      end
+    end
+    
   end
 end
