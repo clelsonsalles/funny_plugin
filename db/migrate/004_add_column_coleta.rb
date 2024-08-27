b46f9dfb546e6ffe90411b32c8d66f42ca1fc9fb
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-08-26 22:58:09 -0300

    Create 004_add_column_coleta.rb

---
+++ db/migrate/004_add_column_coleta.rb
@@ -0,0 +1,9 @@
+class AddColumnColeta < ActiveRecord::Migration[6.1]
+  def up
+    add_column(:coleta, :cnpj, :integer) 
+    add_column(:coleta, :anoInformado, :integer) 
+  end
+
+  def down
+    # no-op
+  end
