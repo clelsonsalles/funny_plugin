9af785f00c74a535fdb3e7dead4ce6671254ad97
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-08-26 22:18:27 -0300

    Update 004_add_references_coleta.rb

--- db/migrate/004_add_references_coleta.rb
+++ db/migrate/004_add_references_coleta.rb
@@ -3,7 +3,9 @@ class AddReferencesColeta < ActiveRecord::Migration[6.1]
     remove_reference(:coleta, :user, foreign_key: true )
     add_reference(:coleta, :usuarioCriacao, class_name: "User", foreign_key: true )
     add_reference(:coleta, :usuarioRealizacao, class_name: "User", foreign_key: true )
-    add_reference(:coleta, :usuarioEnvio, class_name: "User", foreign_key: true )
+    add_reference(:coleta, :usuarioEnvio, class_name: "User", foreign_key: true)
+
+    add_column(:coleta, :cnpj, :integer) 
 
   end
 
