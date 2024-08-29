f4b5232e08c88ccd2543321bbe6f1fbb4ac523f8
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-08-29 17:42:41 -0300

    Update 007_addcolumns_coleta_anual.rb

--- db/migrate/007_addcolumns_coleta_anual.rb
+++ db/migrate/007_addcolumns_coleta_anual.rb
@@ -2,6 +2,23 @@ class AddColumnsColetaAnual < ActiveRecord::Migration[6.1]
 
    def up
     add_column(:coleta, :idEstacao, :integer) 
+    add_column(:coleta, :nEstacao, :integer) 
+    add_column(:coleta, :abertura, :text) 
+    add_column(:coleta, :rua, :text) 
+    add_column(:coleta, :numeroEndereco, :text) 
+    add_column(:coleta, :latitude, :text) 
+    add_column(:coleta, :longitude, :text) 
+    add_column(:coleta, :enlaceMeio, :text) 
+    add_column(:coleta, :enlaceNominal, :text) 
+    add_column(:coleta, :enlaceSwap, :text) 
+    add_column(:coleta, :geometriaWkt, :text) 
+    add_column(:coleta, :srid, :text) 
+    add_column(:coleta, :largCanalUplink, :text) 
+    add_column(:coleta, :capCanalUplink, :text) 
+    add_column(:coleta, :largCanalDownlink, :text) 
+    add_column(:coleta, :capCanalDownlink, :text) 
+    add_column(:coleta, :cnpjContratada, :integer) 
+      
     add_column(:coleta, :idEstacaoOrigem, :integer) 
     add_column(:coleta, :idEstacaoDestino, :integer) 
     add_column(:coleta, :idEnlace, :integer) 
