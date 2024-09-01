7b3e10aac675602d678775409c477df517370982
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-09-01 19:51:02 -0300

    Update export_csv.rb

--- lib/export_csv.rb
+++ lib/export_csv.rb
@@ -67,13 +67,26 @@ module ExportCsv
 
             when "Coleta Anual"
               case coleta.tipoColeta
-                  when ""
-                  
+                  when "estacao"
                         # Adiciona o cabeçalho
-                        csv << ['ID', 'tituloColeta', 'tipoColeta', 'ano', 'mes', 'trimestre', 'uf', 'cidade', 'codigoIBGE', 'tipoCliente', 'tipoAtendimento', 'tipoMeio', 'tipoTecnologia', 'tipoProduto', 'velocidade', 'quantidadeAcesso', 'dadoInformado', 'valor', 'cn']
+                        csv << ['cnpjPrestadora', 'ano', 'idEstacao', 'nEstacao', 'codigoIBGE', 'abertura', 'cep', 'cidade', 'rua', 'numeroEndereco', 'latitude', 'longitude']
                         # Adiciona a coleta
-                        csv << [coleta.id, coleta.tituloColeta, coleta.tipoColeta, coleta.ano, coleta.mes, coleta.trimestre, coleta.uf, coleta.cidade, coleta.codigoIBGE, coleta.tipoCliente, coleta.tipoAtendimento, coleta.tipoMeio, coleta.tipoTecnologia, coleta.tipoProduto, coleta.velocidade, coleta.quantidadeAcesso, coleta.dadoInformado, coleta.valor, coleta.cn]
-
+                        csv << [coleta.cnpjPrestadora, coleta.ano, coleta.idEstacao, coleta.nEstacao, coleta.codigoIBGE, coleta.abertura, coleta.cep, coleta.cidade, coleta.rua, coleta.numeroEndereco, coleta.latitude, coleta.longitude]
+                  when "enlacesProprios"
+                        # Adiciona o cabeçalho
+                        csv << ['cnpjPrestadora', 'ano', 'idEstacaoOrigem', 'idEstacaoDestino', 'idEnlace', 'enlaceMeio', 'enlaceNominal', 'enlaceSwap', 'geometriaWkt', 'srid']
+                        # Adiciona a coleta
+                        csv << [coleta.cnpjPrestadora, coleta.ano, coleta.idEstacaoOrigem, coleta.idEstacaoDestino, coleta.idEnlace, coleta.enlaceMeio, coleta.enlaceNominal, coleta.enlaceSwap, coleta.geometriaWkt, coleta.srid]
+                  when "encalcesContratados"
+                        # Adiciona o cabeçalho
+                        csv << ['cnpjPrestadora', 'ano', 'idEstacaoOrigem', 'idEstacaoDestino', 'idEnlace', 'enlaceMeio', 'cnpjContratada']
+                        # Adiciona a coleta
+                        csv << [coleta.cnpjPrestadora, coleta.ano, coleta.idEstacaoOrigem, coleta.idEstacaoDestino, coleta.idEnlace, coleta.enlaceMeio, coleta.cnpjContratada]
+                  when "encalcesViaSatelite"
+                        # Adiciona o cabeçalho
+                        csv << ['cnpjPrestadora', 'ano', 'idEstacaoOrigem', 'idSatelite', 'codigoSatelite', 'freqUplink', 'freqDownlink', 'largCanalUplink', 'capCanalUplink', 'largCanalDownlink', 'capCanalDownlink']
+                        # Adiciona a coleta
+                        csv << [coleta.cnpjPrestadora, coleta.ano, coleta.idEstacaoOrigem, coleta.idSatelite, coleta.codigoSatelite, coleta.freqUplink, coleta.freqDownlink, coleta.largCanalUplink, coleta.capCanalUplink, coleta.largCanalDownlink, coleta.capCanalDownlink]
                   else
                       csv << [coleta.id, coleta.tituloColeta, coleta.tipoColeta, "TIPO DE COLETA NÃO DEFINIDO"]
               end
