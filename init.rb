4065e36609f76023141ad99a1a903b5aa353111f
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-09-09 17:41:25 -0300

    Update init.rb

--- init.rb
+++ init.rb
@@ -30,8 +30,8 @@ Redmine::Plugin.register :funny_plugin do
 
   delete_menu_item :project_menu, :overview
   delete_menu_item :project_menu, :activity
-  delete_menu_item :project_menu, :issues_calendar
-  delete_menu_item :project_menu, :issues_gantt
+  delete_menu_item :project_menu, :calendar
+  delete_menu_item :project_menu, :gantt
   delete_menu_item :project_menu, :time_entries
   delete_menu_item :project_menu, :issues
   delete_menu_item :project_menu, :news
@@ -41,8 +41,8 @@ Redmine::Plugin.register :funny_plugin do
   delete_menu_item :application_menu, :activity
   delete_menu_item :application_menu, :issues
   delete_menu_item :application_menu, :time_entries
-  delete_menu_item :application_menu, :issues_gantt
-  delete_menu_item :application_menu, :issues_calendar
+  delete_menu_item :application_menu, :gantt
+  delete_menu_item :application_menu, :calendar
   delete_menu_item :application_menu, :news
 
   menu :admin_menu, :cadastrarCliente, { :controller => 'cliente', :action => 'register' }, :caption => 'Novo Analista', :before => :settings
