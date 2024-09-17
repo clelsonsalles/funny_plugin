0f774f236310a5c35f28b4adad074052d8ea01cf
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-09-17 10:58:21 -0300

    Update analista_controller.rb

--- app/controllers/analista_controller.rb
+++ app/controllers/analista_controller.rb
@@ -174,7 +174,7 @@ class AnalistaController < ApplicationController
       coleta.ano = coletaGravada.ano
       coleta.tituloColeta =  coletaGravada.tituloColeta
       coleta.tipoColeta = coletaGravada.tipoColeta
-      coleta.project_id = coletaGravada.id_projeto
+      coleta.project_id = coletaGravada.projeto.id
 
       coleta
 
