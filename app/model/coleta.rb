c81b0bac332e85529e1fe2caf19e3a79dc5507f2
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-09-18 23:10:46 -0300

    Update coleta.rb

--- app/model/coleta.rb
+++ app/model/coleta.rb
@@ -10,7 +10,7 @@ safe_attributes(
       'longitude',          'enlaceMeio',      'enlaceNominal',        'enlaceSwap',             'geometriaWkt',       'srid',  
       'largCanalUplink',    'capCanalUplink',  'largCanalDownlink',    'capCanalDownlink',       'cnpjContratada',     'idEstacaoOrigem', 
       'idEstacaoDestino',   'idEnlace',        'codigoSatelite',        'freqUplink',            'freqDownlink',        'cep', 
-      'idSatelite')
+      'idSatelite',         'semestre')
 
   def self.mensal
      "Coleta Mensal"
