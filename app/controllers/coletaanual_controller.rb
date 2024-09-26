54d908974eeb2d539d9c9787f44a3527f57ca0db
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-09-25 22:39:57 -0300

    Update coletaanual_controller.rb

--- app/controllers/coletaanual_controller.rb
+++ app/controllers/coletaanual_controller.rb
@@ -3,6 +3,7 @@ class ColetaanualController < ApplicationController
     tipoColeta = params[:tipoColeta]
     project_id = params[:project_id]
     ano = params[:ano]
+    titulo = params[:titulo]
 
     paramsColeta = params[:coleta]
     @coleta = Coleta.new    
@@ -14,9 +15,10 @@ class ColetaanualController < ApplicationController
       @coleta.tipoColeta = tipoColeta
       @coleta.project_id = project_id
       @coleta.ano = ano
+      @coleta.titulo - titulo
     end
 
-    @coletas = Coleta.where(tipoColeta:  @coleta.tipoColeta, project_id:  @coleta.project_id, ano: @coleta.ano  )
+    @coletas = Coleta.where(tipoColeta:  @coleta.tipoColeta, project_id:  @coleta.project_id, ano: @coleta.ano, titulo: @coleta.titulo  )
   end
 
  def atualizar
@@ -29,7 +31,7 @@ class ColetaanualController < ApplicationController
 
     @coleta.save
 
-    redirect_to coleta_anual_fazer_path(tipoColeta:  @coleta.tipoColeta, project_id:  @coleta.project_id, ano: @coleta.ano)
+    redirect_to coleta_anual_fazer_path(tipoColeta:  @coleta.tipoColeta, project_id:  @coleta.project_id, ano: @coleta.ano, titulo: @coleta.titulo)
  end
   
 end
