fd01cd13216d89a5b0860a2d13d20156c1d4ad0a
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-09-29 12:35:09 -0300

    Update coleta_controller.rb

--- app/controllers/coleta_controller.rb
+++ app/controllers/coleta_controller.rb
@@ -7,7 +7,7 @@ class ColetaController < ApplicationController
           ano: coleta.ano, mes: coleta.mes, semestre: coleta.semeste, 
           project_id:  coleta.project_id
       )
-      csv_data = ExportCsv.export_coleta_to_csv(coletas)
+      csv_data = ExportCsv.export_coleta_to_csv(coleta.tipoColeta, coletas)
       send_data csv_data, filename: "coleta-#{coleta.project.name}-#{coleta.tituloColeta}-#{coleta.tipoColeta}-#{coleta.textoData}.csv", type: 'text/csv'
 
     end
