0958ba8d09849ea3ac167fda58085b95d0308ff8
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-09-29 12:50:55 -0300

    Update coleta_controller.rb

--- app/controllers/coleta_controller.rb
+++ app/controllers/coleta_controller.rb
@@ -8,6 +8,7 @@ class ColetaController < ApplicationController
           project_id:  coleta.project_id
       )
       Rails.logger.info "Conferindo: coleta-#{coleta.project.name}-#{coleta.tituloColeta}-#{coleta.tipoColeta}-#{coleta.textoData}.csv" 
+      Rails.logger.info "Conferindo where: coleta-#{coleta.tituloColeta}-#{coleta.ano}-#{coleta.mes}-#{coleta.semestre}-#{coleta.project_id}" 
 
       csv_data = ExportCsv.export_coleta_to_csv(coleta.tipoColeta, coletas)
       send_data csv_data, filename: "coleta-#{coleta.project.name}-#{coleta.tituloColeta}-#{coleta.tipoColeta}-#{coleta.textoData}.csv", type: 'text/csv'
