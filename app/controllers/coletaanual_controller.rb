ca9b21b279e432715651092f76070a09031ca9e7
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-10-10 14:46:06 -0300

    Update coletaanual_controller.rb

--- app/controllers/coletaanual_controller.rb
+++ app/controllers/coletaanual_controller.rb
@@ -107,6 +107,38 @@ class ColetaanualController < ApplicationController
       @coleta.tituloColeta = tituloColeta
     end
 
+    #Recupera a última coleta
+    coleta = Coleta.where(tipoColeta:  @coleta.tipoColeta, project_id:  @coleta.project_id, ano: @coleta.ano, tituloColeta: @coleta.tituloColeta ).last
+    if (!coleta.nil?)
+        @coleta = coleta
+        @coleta.id = nil
+    end
+    # Recupera o CNPJ da Organização
+    projeto = Project.find(@coleta.project_id)
+    organizacao = Organizacao.new(projeto)
+    @coleta.cnpj = organizacao.cnpj.tr('^0-9', '')
+
+    require 'net/http'
+    require 'json'
+
+    # URL da API externa de UFs e cidades
+    external_api_url_cidades = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados/'
+    external_api_url_cidades_completa = external_api_url_cidades + params[:coleta][:uf] + "/municipios"
+    uri = URI(external_api_url_cidades_completa)
+    response = Net::HTTP.get(uri)
+
+    array =  JSON.parse(response)    
+    array.each do |municipioJson|
+      if municipioJson["nome"] == params[:coleta][:cidade]
+          @coleta.codigoIBGE = municipioJson["id"]
+          break
+      end
+    end        
+
+    @coleta.uf = params[:coleta][:uf]
+    @coleta.cidade = params[:coleta][:cidade]
+    
+    
     @coletas = Coleta.where(tipoColeta:  @coleta.tipoColeta, project_id:  @coleta.project_id, ano: @coleta.ano, tituloColeta: @coleta.tituloColeta )
   end
 
