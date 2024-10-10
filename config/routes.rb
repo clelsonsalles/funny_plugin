a4441154d337758b3ab86c778caa8eb45e55479c
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-10-10 15:06:02 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -18,29 +18,6 @@ Rails.application.routes.draw do
     get 'coleta/visualizar', :to => 'coleta#visualizar'
     get 'coleta/gerarcsv', :to => 'coleta#gerarcsv'
 
-    get 'coleta/mensalcriar', :to => 'coleta#mensalcriar'
-    post 'coleta/mensalinserir', :to => 'coleta#mensalinserir'
-    post 'coleta/mensalfazer', :to => 'coleta#mensalfazer'
-    post 'coleta/mensalcidadesuf', :to => 'coleta#mensalcidadesuf'
-    post 'coleta/mensalinformar', :to => 'coleta#mensalinformar'
-    post 'coleta/mensalatualizar', :to => 'coleta#mensalatualizar'
-    get 'coleta/mensalvisualizar', :to => 'coleta#mensalvisualizar'
-        
-    get 'coleta/semestralcriar', :to => 'coleta#semestralcriar'
-    post 'coleta/semestralinserir', :to => 'coleta#semestralinserir'
-    post 'coleta/semestralfazer', :to => 'coleta#semestralfazer'
-    post 'coleta/semestralatualizar', :to => 'coleta#semestralatualizar'
-    get 'coleta/semestralvisualizar', :to => 'coleta#semestralvisualizar'
-    
-     
-    get 'coleta/anualcriar', :to => 'coleta#anualcriar'
-    post 'coleta/anualinserir', :to => 'coleta#anualinserir'
-    match 'coleta/anualfazer', :to => 'coleta#anualfazer', via: [:get, :post]
-    post 'coleta/anualcidadesuf', :to => 'coleta#anualcidadesuf'
-    post 'coleta/anualinformar', :to => 'coleta#anualinformar'
-    post 'coleta/anualatualizar', :to => 'coleta#anualatualizar'
-    get 'coleta/anualvisualizar', :to => 'coleta#anualvisualizar'
-
     match 'coleta/anual/fazeruf', :to => 'coletaanual#fazeruf', via: [:get, :post]
     match 'coleta/anual/fazercidadesuf', :to => 'coletaanual#fazercidadesuf', via: [:get, :post, :patch]
     match 'coleta/anual/fazer', :to => 'coletaanual#fazer', via: [:get, :post, :patch]
