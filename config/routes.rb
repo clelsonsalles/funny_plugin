0e797f3700cee2c9c55a1885f8c6f63eec9c6897
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-10-10 15:39:58 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -21,10 +21,10 @@ Rails.application.routes.draw do
     match 'coleta/anual/fazeruf', :to => 'coletaanual#fazeruf', via: [:get, :post]
     match 'coleta/anual/fazercidadesuf', :to => 'coletaanual#fazercidadesuf', via: [:get, :post, :patch]
     match 'coleta/anual/fazer', :to => 'coletaanual#fazer', via: [:get, :post, :patch]
-    post 'coleta/anual/atualizar', :to => 'coletaanual#atualizar', via: [:get, :post, :patch]
+    match 'coleta/anual/atualizar', :to => 'coletaanual#atualizar', via: [:get, :post, :patch]
 
     match 'coleta/semestral/fazer', :to => 'coletasemestral#fazer', via: [:get, :post]
-    post 'coleta/semestral/atualizar', :to => 'coletasemestral#atualizar'
+    match 'coleta/semestral/atualizar', :to => 'coletasemestral#atualizar', via: [:get, :post, :patch]
 
     match 'coleta/mensal/fazeruf', :to => 'coletamensal#fazeruf', via: [:get, :post]
     match 'coleta/mensal/fazercidadesuf', :to => 'coletamensal#fazercidadesuf', via: [:get, :post, :patch]
