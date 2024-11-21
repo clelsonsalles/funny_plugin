b4a1df1804613c7cabad0167b0f866e531801952
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-11-21 17:47:20 -0300

    Update coletamensal_controller.rb

--- app/controllers/coletamensal_controller.rb
+++ app/controllers/coletamensal_controller.rb
@@ -156,8 +156,10 @@ class ColetamensalController < ApplicationController
 
   
   def excluir
-    id_coleta = params[:coleta].id_coleta
+    id_coleta = params[:coleta][:id_coleta]
     Coleta.destroy id_coleta
+    
+    redirect_to cliente_cliente_path
 
 
   end
