53c16430b29b8d9ff6b7250263730c4ab91cde71
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-11-22 09:52:08 -0300

    Update coletamensal_controller.rb

--- app/controllers/coletamensal_controller.rb
+++ app/controllers/coletamensal_controller.rb
@@ -132,8 +132,8 @@ class ColetamensalController < ApplicationController
       else
         coleta = Coleta.find(id_coleta)
       end
+
       @coleta = Coleta.where(tipoColeta:  coleta.tipoColeta, project_id:  coleta.project_id, ano: coleta.ano, mes: coleta.mes).last
-  
       @coletas = Coleta.where(tipoColeta:  @coleta.tipoColeta, project_id:  @coleta.project_id, ano: @coleta.ano, mes: @coleta.mes)
 
   end
@@ -166,7 +166,6 @@ class ColetamensalController < ApplicationController
     Coleta.destroy id_coleta
     
     @coleta = Coleta.where(tipoColeta:  coleta.tipoColeta, project_id:  coleta.project_id, ano: coleta.ano, mes: coleta.mes).last
-    @coletas = Coleta.where(tipoColeta:  @coleta.tipoColeta, project_id:  @coleta.project_id, ano: @coleta.ano, mes: @coleta.mes)
     if @coleta.nil?
         redirect_to cliente_cliente_path
     else
