bdd140968e48a9ba8da64f1b61d47fa01a74b3e5
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2024-11-22 10:20:49 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -25,12 +25,14 @@ Rails.application.routes.draw do
     match 'coleta/anual/editar', :to => 'coletaanual#editar', via: [:get, :post, :patch]
     match 'coleta/anual/excluir', :to => 'coletaanual#excluir', via: [:get, :post, :patch]
     match 'coleta/anual/ver', :to => 'coletaanual#ver', via: [:get, :post, :patch]
+    match 'coleta/anual/salvar', :to => 'coletamensal#salvar', via: [:get, :post, :patch]
 
     match 'coleta/semestral/fazer', :to => 'coletasemestral#fazer', via: [:get, :post]
     match 'coleta/semestral/atualizar', :to => 'coletasemestral#atualizar', via: [:get, :post, :patch]
     match 'coleta/semestral/editar', :to => 'coletasemestral#editar', via: [:get, :post, :patch]
     match 'coleta/semestral/excluir', :to => 'coletasemestral#excluir', via: [:get, :post, :patch]
     match 'coleta/semestral/ver', :to => 'coletasemestral#ver', via: [:get, :post, :patch]
+    match 'coleta/semestral/salvar', :to => 'coletamensal#salvar', via: [:get, :post, :patch]
 
     match 'coleta/mensal/fazeruf', :to => 'coletamensal#fazeruf', via: [:get, :post]
     match 'coleta/mensal/fazercidadesuf', :to => 'coletamensal#fazercidadesuf', via: [:get, :post, :patch]
@@ -39,5 +41,6 @@ Rails.application.routes.draw do
     match 'coleta/mensal/editar', :to => 'coletamensal#editar', via: [:get, :post, :patch]
     match 'coleta/mensal/excluir', :to => 'coletamensal#excluir', via: [:get, :post, :patch]
     match 'coleta/mensal/ver', :to => 'coletamensal#ver', via: [:get, :post, :patch]
+    match 'coleta/mensal/salvar', :to => 'coletamensal#salvar', via: [:get, :post, :patch]
 
 end
